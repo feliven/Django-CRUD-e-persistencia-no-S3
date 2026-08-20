408e4bd0716c852a81f9a4c068e33429feaeaeb8
FVS-MBA <feliven@gmail.com>
2026-08-20 11:33:48 -0300

    inicia criação de nova_imagem, editar_imagem e deletar_imagem

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -32,3 +32,24 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, "galeria/buscar.html", {"cards": fotografias})
+
+
+def nova_imagem(request):
+    return render(
+        request,
+        "galeria/nova_imagem.html",
+    )
+
+
+def editar_imagem(request):
+    return render(
+        request,
+        "galeria/editar_imagem.html",
+    )
+
+
+def deletar_imagem(request):
+    return render(
+        request,
+        "galeria/deletar_imagem.html",
+    )
